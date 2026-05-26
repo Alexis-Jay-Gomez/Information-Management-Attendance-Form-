d7e47c28b44aad222a9cb9879130ec695bda0df0
Alexis-Jay-Gomez <alexisjay396@gmail.com>
2026-05-26 09:24:13 +0800

    v.0.1 ( Startup Page)

--- batibot_attendanceForm/batibot_attendanceForm/Form1.cs
+++ batibot_attendanceForm/batibot_attendanceForm/Form1.cs
@@ -16,5 +16,15 @@ namespace batibot_attendanceForm
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
